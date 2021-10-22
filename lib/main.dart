1c1721bf1d694fa8cc2e09372ff046543890b847
The-Divyansh <divyansh.coding.0602@gmail.com>
2021-10-23 02:07:49 +0530

    Reduced Repeating Code

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void changeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -35,23 +42,13 @@ class _DicePageState extends State<DicePage> {
             Expanded(
               child: FlatButton(
                 child: Image.asset('images/dice$leftDiceNumber.png'),
-                onPressed: () {
-                  setState(() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  });
-                },
+                onPressed: changeDice,
               ),
             ),
             Expanded(
               child: FlatButton(
                 child: Image.asset('images/dice$rightDiceNumber.png'),
-                onPressed: () {
-                  setState(() {
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                  });
-                },
+                onPressed: changeDice,
               ),
             ),
           ],
