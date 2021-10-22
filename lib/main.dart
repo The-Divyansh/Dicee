de5a604c680a1dd132082b53c8c98b96de5df7b5
The-Divyansh <divyansh.coding.0602@gmail.com>
2021-10-23 01:18:12 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice1.png'),
+                onPressed: () {
+                  print('Left Button Pressed!');
+                },
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice2.png'),
+                onPressed: () {
+                  print('Right Button Pressed!');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
